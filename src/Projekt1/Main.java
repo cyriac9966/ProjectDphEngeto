7a148af58d34b2d2335db7a8fd197b836be609ad
Cyriac-Ernest <103205781+cyriac9966@users.noreply.github.com>
2023-01-26 21:47:37 +0100

    Update Main.java

--- src/Projekt1/Main.java
+++ src/Projekt1/Main.java
@@ -3,7 +3,7 @@ package Projekt1;
 public class Main {
 
     public static void main(String[] args) {
-        // nutno zadat do parametru string s polohou ulozeneho souboru
+        // nutno zadat do parametru string s polohou ulozeneho souboru popr nazev souboru ulozeneho v intelij
 	//StateEvidence evidence = StateEvidence.importStates();
     //evidence.printStates();
         //evidence.over20();
